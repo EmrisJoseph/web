fbb3a033f6d86ea489b87e7dd18c335ec5f58391
kayongo johnson brian <kayonbrian@gmail.com>
2019-09-23 13:14:18 +0300

    last changes

--- work/main.js
+++ work/main.js
@@ -38,6 +38,11 @@ function check(){
                  document.getElementById("pic1").style.height='15px';
                 document.getElementById("msg1").innerHTML="Number must only have digits";
               return false; 
+        }else if (mobile.length <8){
+          document.getElementById("pic1").src="images/error.jpg";
+                 document.getElementById("pic1").style.height='15px';
+                document.getElementById("msg1").innerHTML="Number must only have more than 8 digits";
+              return false;
         }
             else if(mobile.length >10){
                 document.getElementById("pic1").src="images/error.jpg";
@@ -74,5 +79,38 @@ function check(){
                  document.getElementById("pic4").style.height='15px';
                   document.getElementById("msg4").innerHTML="Your Passwords must match";
               return false;
-          }
+          } 
         }
+
+    function disp(){
+        if((sessionStorage in window) && window[sessionStorage] !== null ){
+            var name = document.getElementById('use').value;
+            sessionStorage.setItem('username',name);
+        }
+    }
+
+
+function checkusername(){
+    document.getElementById("pic2").src="";
+    document.getElementById("msg2").innerHTML="";
+     var username = document.myForm.UserName.value;
+    if(('localStorage' in window) && window['localStorage'] !== null ){
+        if(username == username.match(/^[a-zA-Z0-9]+$/)){
+            localStorage.setItem('username',username);
+        }else{
+             document.getElementById("pic2").src="images/error.jpg";
+             document.getElementById("pic2").style.height='15px';
+            document.getElementById("msg2").innerHTML="Your Username must only contain letters and digits";
+        }
+    }else {
+        alert('your browser doesnt support local storagevar username  = document.myForm. ');
+    }
+}
+
+function reset(){
+    if(('localStorage' in window) && window['localStorage'] !== null ){
+       localStorage.removeItem('username');
+    }else {
+        alert('your browser doesnt support local storagevar username  = document.myForm. ');
+    }
+}
